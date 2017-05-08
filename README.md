5f332a04e11a2bcfc77c899981932798f315f3ef
Caíque Cardoso <Caque Cardoso>
2017-05-07 22:30:47 -0300

    understanting how works a two ways data binding in react, or how works de bind of javascript with dom. Based in react Components.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # firstReactProject
 My first react project, for study;
+
+
+#This step
+
+Entendendo como funciona o 'TWO WAY DATA BIND' ou seja, ligação entre o javascript e o DOM, no React baseado em componentes.
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ClassComponent from './classComponent'
+import { Field } from './field'
 
 ReactDOM.render(
-    <ClassComponent label='Contator' initialValue={10}/>
+    <Field initialValue='Teste'/>
 , document.getElementById('app'))
